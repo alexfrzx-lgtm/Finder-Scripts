3bccf3f0b228a8528030018d9573b5a67efa7e14
alexfrzx-lgtm <alexfrzx@gmail.com>
2026-02-15 04:23:13 -0500

    Update Notifier&Hopper.lua

--- Notifier&Hopper.lua
+++ Notifier&Hopper.lua
@@ -161,11 +161,8 @@ task.spawn(function()
 end)
 
 -- WEBHOOKS
-local WEBHOOK_50M = "https://discord.com/api/webhooks/1465393299002228858/wJ2z0hQANHLFhCBmyVr3ATFdVG2AzZw_EmkmXd6NpPhcprJx5ppJ2_-otme0ggofFA_m"
+local WEBHOOK_PRIORITY = "https://discord.com/api/webhooks/1465393299002228858/wJ2z0hQANHLFhCBmyVr3ATFdVG2AzZw_EmkmXd6NpPhcprJx5ppJ2_-otme0ggofFA_m"
 local WEBHOOK_SHOWCASE = "https://discord.com/api/webhooks/1472370425123049571/OAlyVoxMrhp4yOP1loAezHF-zkKDWPLDAA29Ko9M2DT7wGHJSTyXj8bImw6vInl4VhoC"
-
--- MINIMOS
-local MIN_PRODUCTION_50M = 50_000_000
 
 -- PINGS
 local PING_HERE_AT = 550_000_000
@@ -188,59 +185,36 @@ if not http_request then return end
 
 local BRAINROT_IMAGES = {
 
--- A
-["Arcadopus"] = "https://www.lolga.com/uploads/images/goods/steal-a-brainrot/all-server-arcadopus.png",
-["Mi Gatito"] = "https://static.wikia.nocookie.net/stealabr/images/5/50/AyMiGatito.png",
-
 -- B
-["Bacuru and Egguru"] = "https://static.wikia.nocookie.net/stealabr/images/b/b5/Bacuru_and_Egguru.png",
-["Brunito Marsito"] = "https://static.wikia.nocookie.net/stealabr/images/6/60/Brunito_Marsito_byLuiko.png",
-["Burrito Bandito"] = "https://static.wikia.nocookie.net/stealabr/images/e/e6/PoTaTo.png",
 ["Burguro And Fryuro"] = "https://static.wikia.nocookie.net/stealabr/images/6/65/Burguro-And-Fryuro.png",
 
 -- C
 ["Capitano Moby"] = "https://static.wikia.nocookie.net/stealabr/images/e/ef/Moby.png",
 ["Celularcini Viciosini"] = "https://media.discordapp.net/attachments/1452514638892634254/1459951125439713303/Celularcini_Viciosini.webp?format=png",
 ["Cerberus"] = "https://static.wikia.nocookie.net/stealabr/images/4/45/Cerberus.png",
-["Chicleteira Bicicleteira"] = "https://static.wikia.nocookie.net/stealabr/images/5/5a/Chicleteira.png",
-["Chicleteirina Bicicleteirina"] = "https://static.wikia.nocookie.net/stealabr/images/a/aa/Chicliterita_bicicliterita.png",
-["Chicleteira Noelteira"] = "https://static.wikia.nocookie.net/stealabr/images/b/b3/Noel.png",
-["Chill Puppy"] = "https://static.wikia.nocookie.net/stealabr/images/3/30/ChillPuppy.png",
 ["Chillin chili"] = "https://static.wikia.nocookie.net/stealabr/images/e/e0/Chilin.png",
-["Chimnino"] = "https://static.wikia.nocookie.net/stealabr/images/c/c5/Chimnino.png",
 ["Chipso and Queso"] = "https://static.wikia.nocookie.net/stealabr/images/f/f8/Chipsoqueso.png",
 ["Cooki and Milki"] = "https://static.wikia.nocookie.net/stealabr/images/9/9b/Cooki_and_milki.png",
-["Cupid Cupid Sahur"] = "https://static.wikia.nocookie.net/stealabr/images/8/89/Cupid_Cupid_Sahur.png",
-["Cupid Hotspot"] = "https://static.wikia.nocookie.net/stealabr/images/1/1f/Pot_Cupid.png",
-["Chicleteira Cupideira"] = "https://static.wikia.nocookie.net/stealabr/images/6/6f/Chicleteira_Cupideira.png",
 
 -- D
-["DonkeyTurbo Express"] = "https://static.wikia.nocookie.net/stealabr/images/9/9a/DonkeyturboExpress.png",
 ["Dragon Cannelloni"] = "https://static.wikia.nocookie.net/stealabr/images/3/31/Nah_uh.png",
 ["Dragon Gingerini"] = "https://static.wikia.nocookie.net/stealabr/images/3/3a/DragonGingerini.png",
 
--- E
-["Esok Sekolah"] = "https://static.wikia.nocookie.net/stealabr/images/2/2a/EsokSekolah2.png",
-["Eviledon"] = "https://static.wikia.nocookie.net/stealabr/images/7/78/Eviledonn.png",
-
 -- F
 ["Festive 67"] = "https://static.wikia.nocookie.net/stealabr/images/c/c8/TransparentFestive67.png",
-["67"] = "https://static.wikia.nocookie.net/stealabr/images/8/83/BOIIIIIII_SIX_SEVEN_%F0%9F%98%82%F0%9F%98%82%F0%9F%98%82%F0%9F%98%82%F0%9F%98%82%F0%9F%98%82%F0%9F%98%82%F0%9F%98%82%F0%9F%98%82%F0%9F%98%82.png",
 ["Fishino Clownino"] = "https://static.wikia.nocookie.net/stealabr/images/d/d6/Fishino_Clownino.png",
 ["Fragrama and Chocrama"] = "https://static.wikia.nocookie.net/stealabr/images/9/9a/Fragrama_and_Chocrama.png",
 
 -- G
 ["Garama and Madundung"] = "https://static.wikia.nocookie.net/stealabr/images/e/ee/Garamadundung.png",
 ["Ginger Gerat"] = "https://static.wikia.nocookie.net/stealabr/images/8/85/GingerGerat.png",
-["Gobblino Uniciclino"] = "https://static.wikia.nocookie.net/stealabr/images/c/c5/Gobblino_Uniciclino.png",
-["Guest 666"] = "https://static.wikia.nocookie.net/stealabr/images/9/99/Guest666t.png",
 
 -- H
 ["Headless Horseman"] = "https://static.wikia.nocookie.net/stealabr/images/f/ff/Headlesshorseman.png",
 ["Hydra Dragon Cannelloni"] = "https://static.wikia.nocookie.net/stealabr/images/e/ee/Hydra_Dragon_Cannelloni.png",
 
 -- J
-["Jolly jolly Sahur"] = "https://static.wikia.nocookie.net/stealabr/images/f/f1/JollyJollySahur.png",
+["Jolly Jolly Sahur"] = "https://static.wikia.nocookie.net/stealabr/images/f/f1/JollyJollySahur.png",
 
 -- K
 ["Ketupat Bros"] = "https://static.wikia.nocookie.net/stealabr/images/4/4d/Ketupat_Bros.png",
@@ -249,53 +223,30 @@ local BRAINROT_IMAGES = {
 
 -- L
 ["La Casa Boo"] = "https://static.wikia.nocookie.net/stealabr/images/d/de/Casa_Booo.png",
-["La Extinct Grande"] = "https://static.wikia.nocookie.net/stealabr/images/c/cd/La_Extinct_Grande.png",
 ["La Ginger Sekolah"] = "https://static.wikia.nocookie.net/stealabr/images/1/14/Esok_Ginger.png",
-["La Grande Combinasion"] = "https://static.wikia.nocookie.net/stealabr/images/d/d8/Carti.png",
 ["La Jolly Grande"] = "https://static.wikia.nocookie.net/stealabr/images/5/5f/La_Chrismas_Grande.png",
 ["La Secret Combinasion"] = "https://static.wikia.nocookie.net/stealabr/images/f/f2/Lasecretcombinasion.png",
-["La Spooky Grande"] = "https://static.wikia.nocookie.net/stealabr/images/5/51/Spooky_Grande.png",
 ["La Supreme Combinasion"] = "https://static.wikia.nocookie.net/stealabr/images/5/52/SupremeCombinasion.png",
 ["La Taco Combinasion"] = "https://static.wikia.nocookie.net/stealabr/images/8/84/Latacocombi.png",
 ["Las Sis"] = "https://cdn.shopify.com/s/files/1/0837/8712/0919/files/Las_Sis_600x600.webp?v=1758288678",
 ["Lavadorito Spinito"] = "https://static.wikia.nocookie.net/stealabr/images/f/ff/Lavadorito_Spinito.png",
-["Los 25"] = "https://static.wikia.nocookie.net/stealabr/images/9/9b/Transparent_Los_25.png",
-["Los 67"] = "https://static.wikia.nocookie.net/stealabr/images/d/db/Los-67.png",
 ["Los Bros"] = "https://static.wikia.nocookie.net/stealabr/images/5/53/BROOOOOOOO.png",
-["Los Burritos"] = "https://static.wikia.nocookie.net/stealabr/images/9/97/LosBurritos.png",
-["Los Candies"] = "https://static.wikia.nocookie.net/stealabr/images/f/f9/Candy%21.png",
-["Los Chicleteiras"] = "https://static.wikia.nocookie.net/stealabr/images/4/4d/Los_ditos.png",
-["Los combinasionas"] = "https://static.wikia.nocookie.net/stealabr/images/3/36/Stop_taking_my_chips_im_just_a_baybeh.png",
 ["Los Hotspotsitos"] = "https://static.wikia.nocookie.net/stealabr/images/6/69/Loshotspotsitos.png",
-["Los Jolly Combinasionas"] = "https://static.wikia.nocookie.net/stealabr/images/7/7b/Los_jollycombos.png",
-["Los Mobilis"] = "https://static.wikia.nocookie.net/stealabr/images/2/27/Losmobil.png",
-["Los nooo My Hotspotsitos"] = "https://cdn.salla.sa/jDznl/003869f6-f451-47df-a720-491c012bfe01-1000x1000-X33W5yduW5d98TIEmdHhWHYgFwI9KCVCrqSIdZaF.png",
 ["Los Planitos"] = "https://static.wikia.nocookie.net/stealabr/images/8/83/Los_Planitos.png",
 ["Los Primos"] = "https://static.wikia.nocookie.net/stealabr/images/9/96/LosPrimos.png",
-["Los Quesadillas"] = "https://static.wikia.nocookie.net/stealabr/images/9/99/LosQuesadillas.png",
 ["Los Spaghettis"] = "https://static.wikitide.net/italianbrainrotwiki/c/cb/Los_spaggetis.webp",
-["Los Spooky Combinasionas"] = "https://static.wikia.nocookie.net/stealabr/images/8/8a/Lospookycombi.png",
 ["Los Tacoritas"] = "https://static.wikia.nocookie.net/stealabr/images/4/40/My_kids_will_also_rob_you.png",
 ["Los Puggies"] = "https://static.wikia.nocookie.net/stealabr/images/c/c8/LosPuggies2.png",
-["Los Mi Gatitos"] = "https://static.wikia.nocookie.net/stealabr/images/a/af/Los_ay_Gattitos.png",
 ["Lovin Rose"] = "https://static.wikia.nocookie.net/stealabr/images/a/ab/LovinRose.png",
 ["Love Love Bear"] = "https://static.wikia.nocookie.net/stealabr/images/b/bf/Love_Love_Bear.png",
 ["La Romantic Grande"] = "https://static.wikia.nocookie.net/stealabr/images/d/d2/LaRomanricGrande_LeakBySammy.png",
 
 -- M
-["Mariachi Corazoni"] = "https://static.wikia.nocookie.net/stealabr/images/5/5a/MariachiCora.png",
 ["Meowl"] = "https://static.wikia.nocookie.net/stealabr/images/b/b8/Clear_background_clear_meowl_image.png",
-["Mieteteira Bicicleteira"] = "https://static.wikia.nocookie.net/stealabr/images/8/86/Mieteteira_Bicicleteira.png",
 ["Money Money Puggy"] = "https://static.wikia.nocookie.net/stealabr/images/0/09/Money_money_puggy.png",
-["Money Money Reindeer"] = "https://static.wikia.nocookie.net/stealabr/images/e/ec/MoneyMoneyReindeer.png",
 
 -- N
 ["Nuclearo Dinossauro"] = "https://static.wikia.nocookie.net/stealabr/images/c/c6/Nuclearo_Dinosauro.png",
-["Noo my Candy"] = "https://static.wikia.nocookie.net/stealabr/images/1/12/Noo_my_candy_transparent.png",
-["Noo my Present"] = "https://static.wikia.nocookie.net/stealabr/images/3/35/Noo_my_Present.png",
-["Naughty Naughty"] = "https://static.wikia.nocookie.net/stealabr/images/1/13/Sought_the_naught.png",
-["Noo my Heart"] = "https://static.wikia.nocookie.net/stealabr/images/7/75/NooMyLoveheart.png",
-
 
 -- O
 ["Orcaledon"] = "https://static.wikia.nocookie.net/stealabr/images/a/a6/Orcaledon.png",
@@ -303,27 +254,19 @@ local BRAINROT_IMAGES = {
 -- P
 ["Popcuru and Fizzuru"] = "https://static.wikia.nocookie.net/stealabr/images/a/a9/Popuru_and_Fizzuru.png",
 
--- Q
-["Quesadilla Vampiro"] = "https://static.wikia.nocookie.net/stealabr/images/0/0e/VampiroQuesa.png", 
-["Quesadilla Crocodila"] = "https://static.wikia.nocookie.net/stealabr/images/3/3f/QuesadillaCrocodilla.png",
-
 -- R
-["Rang Ring Bus"] = "https://static.wikia.nocookie.net/stealabr/images/2/2b/RingRangBus2.png",
 ["Reinito Sleighito"] = "https://static.wikia.nocookie.net/stealabr/images/2/27/Reinito.png",
 ["Rosetti Tualetti"] = "https://static.wikia.nocookie.net/stealabr/images/f/f8/Rossetti_Tualetti.png",
 ["Rosey and Teddy"] = "https://static.wikia.nocookie.net/stealabr/images/9/9b/Rosey_and_Teddy.png",
 
 -- S
 ["Skibidi Toilet"] = "https://static.wikia.nocookie.net/stealabr/images/3/34/Skibidi_toilet.png",
-["Spinny Hammy"] = "https://static.wikia.nocookie.net/stealabr/images/7/7d/SpinnyHammy.png",
 ["Spaghetti Tualetti"] = "https://static.wikia.nocookie.net/stealabr/images/b/b8/Spaghettitualetti.png",
 ["Spooky and Pumpky"] = "https://static.wikia.nocookie.net/stealabr/images/d/d6/Spookypumpky.png",
 ["Strawberry Elephant"] = "https://static.wikia.nocookie.net/stealabr/images/5/58/Strawberryelephant.png",
-["Swag Soda"] = "https://static.wikia.nocookie.net/stealabr/images/9/9f/Swag_Soda.png",
 ["Swaggy Bros"] = "https://static.wikia.nocookie.net/stealabr/images/8/85/Swaggy_Bros.png",
 
 -- T
-["Tacorita Bicicleta"] = "https://static.wikia.nocookie.net/stealabr/images/0/0f/Gonna_rob_you_twin.png",
 ["Tang Tang Keletang"] = "https://static.wikia.nocookie.net/stealabr/images/c/ce/TangTangVfx.png",
 ["Tictac Sahur"] = "https://static.wikia.nocookie.net/stealabr/images/6/6f/Time_moving_slow.png",
 ["Tralaledon"] = "https://static.wikia.nocookie.net/stealabr/images/7/79/Brr_Brr_Patapem.png",
@@ -335,6 +278,7 @@ local BRAINROT_IMAGES = {
 -- local BRAINROT_IMAGES = { ... }
 
 -- Prioridad lista
+
 local PRIORITY = {
 ["Strawberry Elephant"] = 1,
 ["Meowl"] = 2,
@@ -362,7 +306,7 @@ local PRIORITY = {
 ["La Ginger Sekolah"] = 24,
 ["Festive 67"] = 25,
 ["Garama and Madundung"] = 26,
-["Jolly jolly Sahur"] = 27,
+["Jolly Jolly Sahur"] = 27,
 ["Lavadorito Spinito"] = 28,
 ["Ketchuru and Musturu"] = 29,
 ["Rosetti Tualetti"] = 30,
@@ -375,71 +319,22 @@ local PRIORITY = {
 ["La Taco Combinasion"] = 37,
 ["Swaggy Bros"] = 38,
 ["Los Tacoritas"] = 39,
-["La Romantic Grande"] = 40, -- CORREGIDO
+["La Romantic Grande"] = 40,
 ["Los Primos"] = 41,
-["W or L"] = 42,
-["Tralaledon"] = 43,
-["Eviledon"] = 44,
-["Los Puggies"] = 45,
-["Gobblino Uniciclino"] = 46,
-["La Jolly Grande"] = 47,
-["Esok Sekolah"] = 48,
-["Chillin chili"] = 49,
-["Chipso and Queso"] = 50,
-["Money Money Reindeer"] = 51,
-["Los Bros"] = 52,
-["La Spooky Grande"] = 53,
-["Tuff Toucan"] = 54,
-["Bacuru and Egguru"] = 55,
-["Mieteteira Bicicleteira"] = 56,
-["La Extinct Grande"] = 57,
-["Celularcini Viciosini"] = 58,
-["Money Money Puggy"] = 59,
-["Los 67"] = 60,
-["Los Candies"] = 61,
-["Los Spooky Combinasionas"] = 62,
-["Los Jolly Combinasionas"] = 63,
-["Los Mobilis"] = 64,
-["Los Hotspotsitos"] = 65,
-["Las Sis"] = 66,
-["Los Planitos"] = 67,
-["Chicleteira Cupideira"] = 68,
-["Fishino Clownino"] = 69,
-["Tacorita Bicicleta"] = 70,
-["Spinny Hammy"] = 71,
-["Nuclearo Dinossauro"] = 72,
-["Los combinasionas"] = 73,
-["Chicleteira Noelteira"] = 74,
-["Chimnino"] = 75,
-["Noo my Heart"] = 76,
-["Swag Soda"] = 77,
-["Mariachi Corazoni"] = 78,
-["La Grande Combinasion"] = 79,
-["Los 25"] = 80,
-["Los Burritos"] = 81,
-["67"] = 82,
-["DonkeyTurbo Express"] = 83,
-["Los Chicleteiras"] = 84,
-["Guest 666"] = 85,
-["Los Mi Gatitos"] = 86,
-["Rang Ring Bus"] = 87,
-["Noo my Present"] = 89,
-["Los nooo My Hotspotsitos"] = 90,
-["Noo my Candy"] = 91,
-["Arcadopus"] = 92,
-["Los Quesadillas"] = 93,
-["Burrito Bandito"] = 94,
-["Chill Puppy"] = 95,
-["Chicleteirina Bicicleteirina"] = 96,
-["Chicleteira Bicicleteira"] = 97,
-["Brunito Marsito"] = 98,
-["Cupid Hotspot"] = 99,
-["Quesadilla Vampiro"] = 100,
-["Ho Ho Ho Sahur"] = 101,
-["Mi Gatito"] = 102,
-["Cupid Cupid Sahur"] = 103,
-["Quesadilla Crocodila"] = 104,
-["Naughty Naughty"] = 105,
+["La Jolly Grande"] = 42,
+["Chillin chili"] = 43,
+["Chipso and Queso"] = 44,
+["W or L"] = 45,
+["Celularcini Viciosini"] = 46,
+["Los Hotspotsitos"] = 47,
+["Las Sis"] = 48,
+["Los Planitos"] = 49,
+["Fishino Clownino"] = 50,
+["Nuclearo Dinossauro"] = 51,
+["Money Money Puggy"] = 52,
+["Tuff Toucan"] = 53,
+["Los Puggies"] = 54,
+["Tralaledon"] = 55,
 }
 
 -- NORMALIZAR NOMBRES (ANTI BUGS)
@@ -483,22 +378,27 @@ end
 
 -- SCAN (FIX DEFINITIVO PARA x2, x4, x8...)
 
-local function scan(minProduction)
+local function scan()
     local list = {}
 
     for _,ui in ipairs(workspace:GetDescendants()) do
         if ui:IsA("TextLabel") then
             local value = parseProduction(ui.Text)
-            if value and value >= minProduction then
+            if value then
 
                 local parent = ui.Parent
 
                 for _,c in ipairs(parent:GetChildren()) do
                     if c:IsA("TextLabel") and not c.Text:find("%$") then
-                        table.insert(list, {
-                            name = c.Text,
-                            value = value
-                        })
+                        local brainrotName = c.Text
+
+if PRIORITY[brainrotName] then
+    table.insert(list, {
+        name = brainrotName,
+        value = value
+    })
+end
+
                         break
                     end
                 end
@@ -511,7 +411,7 @@ end
 
 -- WEBHOOK
 
-local notified50M = {}
+local notifiedPriority = {}
 local notifiedShowcase = {}
 
 local function send(list, webhook, pingRole, lastHashRef)
@@ -602,12 +502,12 @@ end
 
 local embed = {
     title = "**" .. main.name .. "**",
-    color = 2829618,
+    color = 10485760,
     description = "**— " .. formatMoney(main.value) .. "**\n\n",
     footer = {
         text = "BryAll Notifier "
     },
-    timestamp = os.date("!%Y-%m-%dT%H:%M:%SZ")
+   
 }
 
 
@@ -629,7 +529,7 @@ embed.description = embed.description ..
 
 if hasOthers then
     embed.description = embed.description ..
-        "**Other Brainrots**\n```" .. others .. "```\n\n"
+        "**Others Brainrots**\n```" .. others .. "```\n\n"
 end
 
 -- THUMBNAIL
@@ -654,9 +554,11 @@ http_request({
     })
 })
 
--- SHOWCASE (COPIA DE LA 50M)
-if webhook == WEBHOOK_50M then
+-- SHOWCASE (COPIA DE LA PRIORITY)
+if webhook == WEBHOOK_PRIORITY then
     local showcaseEmbed = HttpService:JSONDecode(HttpService:JSONEncode(embed))
+
+showcaseEmbed.color = 16766720
 
     -- QUITAR COMPLETAMENTE SERVER ID Y JOIN SERVER
     if showcaseEmbed.description then
@@ -708,7 +610,9 @@ label.Parent = screenGui
 
 task.spawn(function()
     while true do
-        send(scan(MIN_PRODUCTION_50M), WEBHOOK_50M, false, notified50M)
+        send(scan(), WEBHOOK_PRIORITY, false, notifiedPriority)
         task.wait(SCAN_DELAY)
     end
 end)
+
+  
